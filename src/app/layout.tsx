b95ca0d91f21be42659b2e1e3af4eb596d559c6d
Kostenko Andrey <andrey@Mac.fibertel.com.ar>
2025-06-10 14:14:03 -0300

    add font Roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import './globals.css'
+import React from 'react';
+import { Roboto } from 'next/font/google';
+import './globals.css';
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className={roboto.className}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
